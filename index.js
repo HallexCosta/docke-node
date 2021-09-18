ecd3bd258297d31d0aaaff033879d8082e05076d
HallexCosta <hallex.costa@hotmail.com>
2021-09-18 15:34:23 -0300

    add hello docker compose

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const host = "0.0.0.0";
 const port = 3333;
 
 app.get("/", (_, response) => {
-  response.send("Hello Docker");
+  response.send("Hello Docker Compose");
 });
 
 app.listen(port, host);
