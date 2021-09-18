d53fa246b3871edbef9b7eb41ebdf1e0ffeb4a3c
HallexCosta <hallex.costa@hotmail.com>
2021-09-17 21:19:45 -0300

    add simple route with express

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+const host = "0.0.0.0";
+const port = 3333;
+
+app.get("/", (_, response) => {
+  response.send("Hello Docker");
+});
+
+app.listen(port, host);
